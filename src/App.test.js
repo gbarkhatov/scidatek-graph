883d6ef6b5c17f5304c9c5cdbe3d56a560a57663
Govard Barkhatov <gbarkhatov@gmail.com>
2021-02-18 16:28:39 +1000

    add app tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders CompletedSignUps properly', () => {
   render(<App />)
   const linkElement = screen.getByText(/Sign-ups completed/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+test('renders NodeMapView properly', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/Flow dropout per step and service/i)
   expect(linkElement).toBeInTheDocument()
 })
